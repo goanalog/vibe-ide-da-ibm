17e88823e3270c668e0107d7b1f2ff55d5182cc4
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-24 01:28:31 -0500

    jbk

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  description = "IBM Cloud region (e.g., us-south)"
+  type        = string
+  default     = "us-south"
+}
+
+variable "bucket_prefix" {
+  description = "Prefix for the COS bucket"
+  type        = string
+  default     = "vibe-bucket"
+}
+
+variable "website_index" {
+  description = "Index file for website hosting"
+  type        = string
+  default     = "index.html"
+}
+
+variable "website_error" {
+  description = "Error file for website hosting"
+  type        = string
+  default     = "404.html"
+}

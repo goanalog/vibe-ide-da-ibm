c0eadf655b3b815d646faa1fbc0526c11b27cbc5
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-24 00:33:19 -0500

    bjk

--- main.tf
+++ main.tf
@@ -1,48 +1,48 @@
-resource "ibm_resource_instance" "cos_instance" { [cite: 2]
-  name     = "vibe-instance" [cite: 2]
-  service  = "cloud-object-storage" [cite: 2]
-  plan     = "lite" [cite: 2]
-  location = "global" [cite: 2]
+resource "ibm_resource_instance" "cos_instance" {
+  name     = "vibe-instance"
+  service  = "cloud-object-storage"
+  plan     = "lite"
+  location = "global"
 }
 
-resource "random_string" "suffix" { [cite: 2]
-  length  = 6 [cite: 2]
-  upper   = false [cite: 2]
-  special = false [cite: 2]
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  special = false
 }
 
-resource "ibm_cos_bucket" "vibe_bucket" { [cite: 2]
-  bucket_name          = "${var.bucket_prefix}-${random_string.suffix.result}" [cite: 2]
-  resource_instance_id = ibm_resource_instance.cos_instance.id [cite: 2]
-  storage_class        = "standard" [cite: 2]
-  force_delete         = true [cite: 2]
+resource "ibm_cos_bucket" "vibe_bucket" {
+  bucket_name          = "${var.bucket_prefix}-${random_string.suffix.result}"
+  resource_instance_id = ibm_resource_instance.cos_instance.id
+  storage_class        = "standard"
+  force_delete         = true
 }
 
-resource "ibm_cos_bucket_website_configuration" "vibe_bucket_website" { [cite: 2]
-  bucket_crn      = ibm_cos_bucket.vibe_bucket.crn 
-  bucket_location = var.region 
+resource "ibm_cos_bucket_website_configuration" "vibe_bucket_website" {
+  bucket_crn      = ibm_cos_bucket.vibe_bucket.crn
+  bucket_location = var.region
 
-  website_configuration { 
-    index_document { 
-      suffix = var.website_index 
+  website_configuration {
+    index_document {
+      suffix = var.website_index
     }
 
-    error_document { 
-      key = var.website_error 
+    error_document {
+      key = var.website_error
     }
   }
 }
 
-resource "ibm_cos_bucket_object" "index_html" { 
-  bucket_crn      = ibm_cos_bucket.vibe_bucket.crn 
-  bucket_location = var.region 
-  key             = var.website_index 
-  content         = file("${path.module}/sample-app/index.html") 
+resource "ibm_cos_bucket_object" "index_html" {
+  bucket_crn      = ibm_cos_bucket.vibe_bucket.crn
+  bucket_location = var.region
+  key             = var.website_index
+  content         = file("${path.module}/sample-app/index.html")
 }
 
-resource "ibm_cos_bucket_object" "error_html" { 
-  bucket_crn      = ibm_cos_bucket.vibe_bucket.crn [cite: 4]
-  bucket_location = var.region [cite: 4]
-  key             = var.website_error [cite: 4]
-  content         = file("${path.module}/sample-app/404.html") [cite: 4]
+resource "ibm_cos_bucket_object" "error_html" {
+  bucket_crn      = ibm_cos_bucket.vibe_bucket.crn
+  bucket_location = var.region
+  key             = var.website_error
+  content         = file("${path.module}/sample-app/404.html")
 }

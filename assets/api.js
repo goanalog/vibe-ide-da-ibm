e9432551ec8379712aa8d8901999f90fd1932ed6
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-24 14:46:51 -0500

    asd

--- assets/api.js
+++
@@ -1,6 +0,0 @@
-// assets/api.js
-export async function fetchManifest(url) {
-  const res = await fetch(url, { headers: { "accept": "application/json" }});
-  if (!res.ok) throw new Error(`HTTP ${res.status}`);
-  return res.json();
-}
--- assets/env.js
+++
@@ -1,4 +0,0 @@
-// assets/env.js
-window.__VIBE_ENV__ = {
-  NOTE: "Static starter; function endpoint is printed from Terraform outputs."
-};

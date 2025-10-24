0f8c7718f2ee52c625ba021904d15ce1530dfa7c
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-24 13:07:29 -0500

    afsd

--- outputs.tf
+++ outputs.tf
@@ -3,13 +3,13 @@
 ###############################################################################
 
 output "primaryoutputlink" {
-  value       = "https://${ibm_cos_bucket.vibe_bucket.website_endpoint}"
+  value       = ibm_cos_bucket_website_configuration.vibe_site.website_url
   description = "Vibe IDE URL"
 }
 
-output "code_engine_project_name" {
-  value       = ibm_code_engine_project.vibe_ce_project.name
-  description = "Name of the Code Engine project created for backend extensions"
+output "vibe_bucket_website_endpoint" {
+  value       = ibm_cos_bucket_website_configuration.vibe_site.website_url
+  description = "COS Website Endpoint"
 }
 
 output "vibe_bucket_name" {

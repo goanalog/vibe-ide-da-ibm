5eb33477cd40519d9ed3b721ae488ee50c6940e2
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-24 18:27:29 -0500

    asfd

--- assets/env.tmpl.js
+++
@@ -1,8 +0,0 @@
-// assets/env.tmpl.js
-// Populated by Terraform or your CI. Placeholder defaults shown.
-window.__VIBE_ENV__ = {
-  APPID_CLIENT_ID: "${APPID_CLIENT_ID}",
-  APPID_TENANT_ID: "${APPID_TENANT_ID}",
-  APP_REGION: "${APP_REGION}",
-  PUSH_API_URL: "${PUSH_API_URL}" // e.g., https://your-app.XXXX.codeengine.appdomain.cloud/publish
-};

12ec74ca126e3fe15c192a069e77e16f0e946d04
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-25 13:31:19 -0500

    fsa

--- www/assets/env.tmpl.js
+++
@@ -1,7 +0,0 @@
-// Terraform will template these values during deployment.
-window.__VIBE_ENV__ = {
-  APPID_CLIENT_ID: "${APPID_CLIENT_ID}",
-  APPID_TENANT_ID: "${APPID_TENANT_ID}",
-  APP_REGION: "${APP_REGION}",
-  PUSH_API_URL: "${PUSH_API_URL}"
-};

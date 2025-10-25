8f87de62169551cf7da3d2ce065fb57d056874b4
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-25 17:08:54 -0500

    asfd

--- outputs.tf
+++
@@ -1,2 +0,0 @@
-output "primaryoutputlink" { description = "Public URL to the live sample page" value = local.primary_outputlink }
-output "push_api_url"      { description = "If set, front-end Push 🚀 posts here. Leave empty to disable." value = local.push_api_url }

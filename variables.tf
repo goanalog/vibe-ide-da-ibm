f66fbe957822a5599e82e9855aebefe161dd0c41
goanalog <brendanandrewfitzpatrick@gmail.com>
2025-10-25 17:00:50 -0500

    fsd

--- variables.tf
+++ variables.tf
@@ -1,4 +1,19 @@
-variable "region" { type = string  default = "us-south" }
-variable "primary_output_path" { type = string  default = "sample.html" }
-variable "primary_output_host" { type = string  default = "https://example.invalid" }
-variable "push_api_url" { type = string  default = "" }
+variable "region" {
+  type    = string
+  default = "us-south"
+}
+
+variable "primary_output_path" {
+  type    = string
+  default = "sample.html"
+}
+
+variable "primary_output_host" {
+  type    = string
+  default = "https://example.invalid"
+}
+
+variable "push_api_url" {
+  type    = string
+  default = ""
+}
